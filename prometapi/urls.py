6562cfdafbfd7d2aa6ea8033f4773388d289d499
Gasper Zejn <zejn@kiberpipa.org>
2016-02-18 09:45:48 +0100

    Fix compatibility warnings.

--- prometapi/urls.py
+++ prometapi/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
+from prometapi import views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,7 +8,7 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'prometapi.views.home', name='home'),
+    # url(r'^$', views.home, name='home'),
     # url(r'^prometapi/', include('prometapi.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -16,12 +17,12 @@ urlpatterns = [
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^promet/$', RedirectView.as_view(url='https://github.com/zejn/prometapi/blob/master/README.rst')),
-    url(r'^promet/events/', 'prometapi.views.events'),
-    url(r'^promet/burja/', 'prometapi.views.burja'),
-    url(r'^promet/burjaznaki/', 'prometapi.views.burjaznaki'),
-    url(r'^promet/counters/', 'prometapi.views.counters'),
-    url(r'^promet/parkirisca/lpt/', 'prometapi.views.parkirisca_lpt'),
-    url(r'^promet/bicikelj/list/', 'prometapi.views.bicikelj'),
-    url(r'^promet/gk_to_wgs84/', 'prometapi.views.gk_to_wgs84'),
-    url(r'^sos112/spin/', 'prometapi.views.sos112'),
+    url(r'^promet/events/', views.events),
+    url(r'^promet/burja/', views.burja),
+    url(r'^promet/burjaznaki/', views.burjaznaki),
+    url(r'^promet/counters/', views.counters),
+    url(r'^promet/parkirisca/lpt/', views.parkirisca_lpt),
+    url(r'^promet/bicikelj/list/', views.bicikelj),
+    url(r'^promet/gk_to_wgs84/', views.gk_to_wgs84),
+    url(r'^sos112/spin/', views.sos112),
 ]
